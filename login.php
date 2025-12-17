600556f1e6f5513529fb09b2ff1814b0895effbb
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-17 10:07:56 +0545

    login page

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+include 'header.php';?>
+<div id="login_page">
+    <form action="login_process.php" method="post">
+        <div>
+            <label>Username</label>
+            <input type="text" name="username" id="">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" id="">
+        </div>
+        <div>
+            <input type="submit" value="login">
+        </div>
+    </form>
+</div>
