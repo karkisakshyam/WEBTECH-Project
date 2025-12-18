c0dcd33595806d02dca4afa0b9d14b16bc2d5aa9
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-18 09:52:53 +0545

    page

--- login_process.php
+++ login_process.php
@@ -4,8 +4,13 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if($username == "admin" & $password == "123"){
+    $_SESSION['user'] = 'admin';
     header ("Location: dashboard.php");
-}else{
+}else if ($username == "customer" & $password == "123"){
+$_SESSION['user'] = 'customer';
+    header ("Location: dashboard.php");
+}
+else{
     echo "Invalid credentials. Please try again.";
 }
 ?>
