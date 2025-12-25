4ffee5e5b759c770c6d95d1dc4d7bbbba6591a10
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-25 09:56:19 +0545

    small update

--- login_process.php
+++ login_process.php
@@ -1,16 +1,36 @@
 <?php
+require "database.php";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+session_start(); // Must be at the very top
 
-if($username == "admin" & $password == "123"){
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+
+
+
+$hashed_password = password_hash($password,PASSWORD_DEFAULT);
+
+
+$query = "select count(*) from users where email = '$email' and password = '$hashed_password'";
+
+$res = mysqli_query($db_connection, $query);
+
+if ($username === "admin" && $password === "123") {
+
     $_SESSION['user'] = 'admin';
-    header ("Location: dashboard.php");
-}else if ($username == "customer" & $password == "123"){
-$_SESSION['user'] = 'customer';
-    header ("Location: dashboard.php");
-}
-else{
+
+    header("Location: dashboard.php");
+    exit;
+
+} elseif ($username === "customer" && $password === "123") {
+
+    $_SESSION['user'] = 'customer';
+    header("Location: dashboard.php");
+    exit;
+
+} else {
+
     echo "Invalid credentials. Please try again.";
 }
 ?>
