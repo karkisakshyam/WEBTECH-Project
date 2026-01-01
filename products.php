0046bd5421c666a4c72d82a4ccb9f5e81d621801
Sakshyam karki <karkisakshyam5@gmail.com>
2026-01-01 19:09:52 +0545

    new

---
+++ products.php
@@ -0,0 +1,19 @@
+<?php
+include 'header.php';
+require "database.php";
+?>
+<div class="content">
+    <h2>Products</h2>
+    <div class="product_container">
+        <?php
+        $query = "SELECT * FROM products";
+        $result = mysqli_query($db_connection, $query);
+
+            while($products = mysqli_fetch_assoc($result)) {
+                ?>
+                <div class='product-item'>
+                <h3><?php echo $products['product_name']; ?></h3>
+                <p><?php echo $products['price']; ?></p>
+                <p><?php echo $products['description']; ?></p>
+                </div>
+            <?php }?>
